6f42a5b87d159416e70e6edf96b78deedff48d92
Nikolay Takev <nikolay.takev1017@gmail.com>
2020-06-02 00:46:31 +0300

    Initial commit

---
+++ src/test/java/com/puprojectA/PuProjectAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puprojectA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuProjectAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
